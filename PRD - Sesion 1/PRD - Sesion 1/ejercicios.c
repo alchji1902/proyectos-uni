477b17fca1163d58ec488f2b7c6eddf0bbaab359
Alex <alex.chillonj@gmail.com>
2026-02-27 21:11:05 +0100

    He fet nomes el 1 una part

--- PRD - Sesion 1/PRD - Sesion 1/ejercicios.c
+++ PRD - Sesion 1/PRD - Sesion 1/ejercicios.c
@@ -12,9 +12,29 @@ void sesion1_ej1()
 {
     /* Completar con el código del ejercicio 1 */
     char a, b, c;
+    int any;
+    int edad;
+    
+    char a1, b1, c1;
+    int edad1;
+    
+    printf("Introduce tu anyo de nacimiento: ");
+    scanf(" %d", &any);
+    
+    
     printf("Introduce tus tres iniciales: ");
     scanf(" %c %c %c", &a, &b, &c);
-    printf("Hola %c%c%c", a, b, c);
+    
+    
+    edad = 2030 - any;
+    
+    edad1 = cambiar_color(YELLOW);
+    a1 = cambiar_color(RED);
+    b1 = cambiar_color(RED);
+    c1 = cambiar_color(RED);
+    
+    printf("Hola %c%c%c\n", a, b, c);
+    printf("El 31/DIC/2030 tendras %d anys\n", edad);
     
 }
 
