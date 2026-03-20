2ea6deae0e0785e73aa1621b0c563573d1340dc3
Alex <alex.chillonj@gmail.com>
2026-03-20 20:56:04 +0100

    PRD4

--- PRD - Sesion 4/PRD - Sesion 4/sesion4_ej3.c
+++ PRD - Sesion 4/PRD - Sesion 4/sesion4_ej3.c
@@ -14,11 +14,11 @@
 int menu() 
 {
     unsigned int val = 0;
-    printf("********** MENU **********");
-    printf("1. Calcular potencia");
-    printf("2. Calcular factorial");
-    printf("3. Calcular numero combinatorio");
-    printf("4. Salir");
+    printf("********** MENU **********\n");
+    printf("1. Calcular potencia\n");
+    printf("2. Calcular factorial\n");
+    printf("3. Calcular numero combinatorio\n");
+    printf("4. Salir\n\n");
     
     printf("Escoja una opcion: ");
     scanf("%ud", &val);
@@ -57,7 +57,9 @@ float factorial(unsigned int arg)
 
 void sesion4_ej3()
 {
-    unsigned int opt, arg;
+    unsigned int opt, arg, p;
+    
+    unsigned long long factorial;
     
     float real;
     
@@ -65,6 +67,28 @@ void sesion4_ej3()
     
     
     opt = menu();
+    
+    while (opt != 4)
+    {
+        if (opt == 1){
+            printf("Ha escogido la opcion calcular potencia\n");
+            p = potencia();
+        }
+        else if (opt == 2){
+            printf("Ha escogido la opcion calcular factorial\n");
+            
+            factorial = factorial();
+        }
+        else if (opt == 3){
+            printf("Opt 3");
+        }
+        else{
+            printf("Opcion no valida!");
+        }
+        
+    }
+    
+    
     
     
     
