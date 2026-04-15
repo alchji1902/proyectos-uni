e26066491dca5e926f1ce149850c339127cf9e62
Alex <alex.chillonj@gmail.com>
2026-04-15 21:21:48 +0200

    mac

---
+++ Teoria/Teoria tema 3/main.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: alexch
+ *
+ * Created on 14 d’abril de 2026, 12:10
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
+
+
+
+
+int vec[80] = {1,2,3,2};
+
+int *vector = &vec;
+
+
+printf("%d", *vec[0]);
